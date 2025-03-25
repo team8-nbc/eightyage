660a1e001555c31891ebfa55aac4bb17a36b4c7d
queenriwon <queenriwon3@gmail.com>
2025-03-25 21:49:50 +0900

    refactor(auth): Builder 사용
    ### 수정한 점
    - 생성자의 파라미터 순서를 보장하기 위해 builder 사용

--- src/main/java/com/example/eightyage/domain/auth/dto/response/AuthAccessTokenResponseDto.java
+++ src/main/java/com/example/eightyage/domain/auth/dto/response/AuthAccessTokenResponseDto.java
@@ -1,9 +1,11 @@
 package com.example.eightyage.domain.auth.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class AuthAccessTokenResponseDto {
 
