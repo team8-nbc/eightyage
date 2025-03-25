16c2a8173cc821887b939356a919dc3b29234423
queenriwon <queenriwon3@gmail.com>
2025-03-25 21:56:18 +0900

    refactor(auth): AuthServiceTest Builder 적용
    ### 수정한 점
    - 생성자의 파라미터 순서를 보장하기 위해 builder 적용
    - 에러 메세지를 ErrorMessage로 관리하고 에러메세지 결과 비교

--- src/test/java/com/example/eightyage/domain/auth/service/AuthServiceTest.java
+++ src/test/java/com/example/eightyage/domain/auth/service/AuthServiceTest.java
@@ -8,6 +8,7 @@ import com.example.eightyage.domain.user.entity.UserRole;
 import com.example.eightyage.domain.user.service.UserService;
 import com.example.eightyage.global.exception.BadRequestException;
 import com.example.eightyage.global.exception.UnauthorizedException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -18,6 +19,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDateTime;
 
+import static com.example.eightyage.global.exception.ErrorMessage.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -36,22 +38,55 @@ public class AuthServiceTest {
     @InjectMocks
     private AuthService authService;
 
+    private AuthSignupRequestDto successSignupDto;
+    private AuthSignupRequestDto passwordCheckErrorSignupDto;
+    private AuthSigninRequestDto successSigninDto;
+    private User user;
+
+    @BeforeEach
+    public void setUp() {
+        passwordCheckErrorSignupDto = AuthSignupRequestDto.builder()
+                .email("email@email.com")
+                .nickname("nickname")
+                .password("password1234")
+                .passwordCheck("password1234!")
+                .userRole("USER_ROLE")
+                .build();
+
+        successSignupDto = AuthSignupRequestDto.builder()
+                .email("email@email.com")
+                .nickname("nickname")
+                .password("password1234")
+                .passwordCheck("password1234")
+                .userRole("USER_ROLE")
+                .build();
+
+        successSigninDto = AuthSigninRequestDto.builder()
+                .email("email@email.com")
+                .password("password1234")
+                .build();
+
+        user = User.builder()
+                .email(successSignupDto.getEmail())
+                .nickname(successSignupDto.getNickname())
+                .userRole(UserRole.ROLE_USER)
+                .build();
+
+    }
+
     @Test
     void 회원가입_비밀번호_확인_불일치_실패() {
         // given
-        AuthSignupRequestDto passwordCheckErrorSignupDto = new AuthSignupRequestDto("email@email.com", "nickname", "password1234", "password12341", "USER_ROLE");
 
         // when & then
-        assertThrows(BadRequestException.class,
-                () -> authService.signup(passwordCheckErrorSignupDto),
-                "비밀번호가 비밀번호 확인과 일치하지 않습니다.");
+        BadRequestException badRequestException = assertThrows(BadRequestException.class,
+                () -> authService.signup(passwordCheckErrorSignupDto));
+        assertEquals(badRequestException.getMessage(), PASSWORD_CONFIRMATION_MISMATCH.getMessage());
     }
 
     @Test
     void 회원가입_성공() {
         // given
-        AuthSignupRequestDto successSignupDto = new AuthSignupRequestDto("email@email.com", "nickname", "password1234", "password1234", "USER_ROLE");
-        User user = new User(1L, successSignupDto.getEmail(), successSignupDto.getNickname(), UserRole.ROLE_USER);
         String accessToken = "accessToken";
         String refreshToken = "refreshToken";
 
@@ -70,51 +105,45 @@ public class AuthServiceTest {
     @Test
     void 로그인_삭제된_유저의_이메일일_경우_실패() {
         // given
-        AuthSigninRequestDto seccessSigninDto = new AuthSigninRequestDto("email@email.com", "password1234");
-        User user = new User(1L, seccessSigninDto.getEmail(), "nickname", UserRole.ROLE_USER);
         ReflectionTestUtils.setField(user, "deletedAt", LocalDateTime.now());
 
         given(userService.findUserByEmailOrElseThrow(any(String.class))).willReturn(user);
 
         // when & then
-        assertThrows(UnauthorizedException.class,
-                () -> authService.signin(seccessSigninDto),
-                "탈퇴한 유저 이메일입니다.");
+        UnauthorizedException unauthorizedException = assertThrows(UnauthorizedException.class,
+                () -> authService.signin(successSigninDto));
+        assertEquals(unauthorizedException.getMessage(), DEACTIVATED_USER_EMAIL.getMessage());
     }
 
     @Test
     void 로그인_비밀번호가_일치하지_않을_경우_실패() {
         // given
-        AuthSigninRequestDto seccessSigninDto = new AuthSigninRequestDto("email@email.com", "password1234");
-        User user = new User(1L, seccessSigninDto.getEmail(), "nickname", UserRole.ROLE_USER);
         ReflectionTestUtils.setField(user, "deletedAt", null);
 
         given(userService.findUserByEmailOrElseThrow(any(String.class))).willReturn(user);
-        given(passwordEncoder.matches(seccessSigninDto.getPassword(), user.getPassword())).willReturn(false);
+        given(passwordEncoder.matches(successSigninDto.getPassword(), user.getPassword())).willReturn(false);
 
         // when & then
-        assertThrows(UnauthorizedException.class,
-                () -> authService.signin(seccessSigninDto),
-                "잘못된 비밀번호입니다.");
+        UnauthorizedException unauthorizedException = assertThrows(UnauthorizedException.class,
+                () -> authService.signin(successSigninDto));
+        assertEquals(unauthorizedException.getMessage(), INVALID_PASSWORD.getMessage());
     }
 
     @Test
     void 로그인_성공() {
         // given
-        AuthSigninRequestDto seccessSigninDto = new AuthSigninRequestDto("email@email.com", "password1234");
-        User user = new User(1L, seccessSigninDto.getEmail(), "nickname", UserRole.ROLE_USER);
         ReflectionTestUtils.setField(user, "deletedAt", null);
 
         String accessToken = "accessToken";
         String refreshToken = "refreshToken";
 
         given(userService.findUserByEmailOrElseThrow(any(String.class))).willReturn(user);
-        given(passwordEncoder.matches(seccessSigninDto.getPassword(), user.getPassword())).willReturn(true);
+        given(passwordEncoder.matches(successSigninDto.getPassword(), user.getPassword())).willReturn(true);
         given(tokenService.createAccessToken(any(User.class))).willReturn(accessToken);
         given(tokenService.createRefreshToken(any(User.class))).willReturn(refreshToken);
 
         // when
-        AuthTokensResponseDto result = authService.signin(seccessSigninDto);
+        AuthTokensResponseDto result = authService.signin(successSigninDto);
 
         // then
         assertEquals(accessToken, result.getAccessToken());
@@ -124,7 +153,6 @@ public class AuthServiceTest {
     @Test
     void 토큰_재발급_성공() {
         // given
-        User user = new User(1L, "email@email.com", "nickname", UserRole.ROLE_USER);
         String refreshToken = "refreshToken";
 
         String reissuedAccessToken = "reissued-accessToken";
