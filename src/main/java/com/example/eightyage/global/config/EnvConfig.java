40518fdef4a6366059019817bdd01adbc8ab16b7
이지은 <tori@ryanlab.kr>
2025-03-27 16:21:33 +0900

    chore: AWS, JWT 환경변수 system properties 추가

--- src/main/java/com/example/eightyage/global/config/EnvConfig.java
+++ src/main/java/com/example/eightyage/global/config/EnvConfig.java
@@ -14,5 +14,8 @@ public class EnvConfig {
         System.setProperty("DB_URL", dotenv.get("DB_URL"));
         System.setProperty("DB_USER", dotenv.get("DB_USER"));
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("DB_URL", dotenv.get("AWS_ACCESS_KEY"));
+        System.setProperty("DB_USER", dotenv.get("AWS_SECRET_KEY"));
+        System.setProperty("DB_PASSWORD", dotenv.get("JWT_SECRET_KEY"));
     }
 }
