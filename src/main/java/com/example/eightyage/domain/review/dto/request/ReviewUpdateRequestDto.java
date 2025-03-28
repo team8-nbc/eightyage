0e010b5789be7e5d09c534352d2692f93123c084
이지은 <tori@ryanlab.kr>
2025-03-28 11:48:49 +0900

    test(review): 리뷰 CRUD 테스트 코드 작성 #28

--- src/main/java/com/example/eightyage/domain/review/dto/request/ReviewUpdateRequestDto.java
+++ src/main/java/com/example/eightyage/domain/review/dto/request/ReviewUpdateRequestDto.java
@@ -2,8 +2,10 @@ package com.example.eightyage.domain.review.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 @Getter
+@AllArgsConstructor
 public class ReviewUpdateRequestDto {
 
     private Double score;
