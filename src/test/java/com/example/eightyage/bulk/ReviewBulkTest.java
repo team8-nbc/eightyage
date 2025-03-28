a775d9db4ba117347d3ded7b89ecf3d33ffd9597
queenriwon <queenriwon3@gmail.com>
2025-03-28 15:35:23 +0900

    test(search): CI 빌드 실페 테스트
    ### 구현한 것
    - 더미데이터 생성 볼륨을 줄여 CI 빌드 실패가 되는 점을 수정

--- src/test/java/com/example/eightyage/bulk/ReviewBulkTest.java
+++ src/test/java/com/example/eightyage/bulk/ReviewBulkTest.java
@@ -1,50 +1,50 @@
-package com.example.eightyage.bulk;
-
-import com.example.eightyage.domain.review.entity.Review;
-import com.example.eightyage.domain.review.repository.ReviewBulkRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@SpringBootTest
-@Profile(value = "ci")
-public class ReviewBulkTest {
-
-    @Autowired
-    private ReviewBulkRepository reviewBulkRepository;
-
-    @Test
-    void 리뷰_더미데이터_생성() {
-
-        List<Review> batchList = new ArrayList<>();
-
-        for (int i = 0; i < 1000; i++) {
-            Review review = new Review();
-            batchList.add(review);
-
-            if (batchList.size() == 1000) {
-                reviewBulkRepository.bulkInsertReviews(batchList);
-                batchList.clear();
-
-                sleep(500);
-            }
-        }
-
-        if (!batchList.isEmpty()) {
-            reviewBulkRepository.bulkInsertReviews(batchList);
-            batchList.clear();
-        }
-    }
-
-    private static void sleep(int millis) {
-        try {
-            Thread.sleep(millis);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-}
+//package com.example.eightyage.bulk;
+//
+//import com.example.eightyage.domain.review.entity.Review;
+//import com.example.eightyage.domain.review.repository.ReviewBulkRepository;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.annotation.Profile;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@SpringBootTest
+//@Profile(value = "ci")
+//public class ReviewBulkTest {
+//
+//    @Autowired
+//    private ReviewBulkRepository reviewBulkRepository;
+//
+//    @Test
+//    void 리뷰_더미데이터_생성() {
+//
+//        List<Review> batchList = new ArrayList<>();
+//
+//        for (int i = 0; i < 1000; i++) {
+//            Review review = new Review();
+//            batchList.add(review);
+//
+//            if (batchList.size() == 1000) {
+//                reviewBulkRepository.bulkInsertReviews(batchList);
+//                batchList.clear();
+//
+//                sleep(500);
+//            }
+//        }
+//
+//        if (!batchList.isEmpty()) {
+//            reviewBulkRepository.bulkInsertReviews(batchList);
+//            batchList.clear();
+//        }
+//    }
+//
+//    private static void sleep(int millis) {
+//        try {
+//            Thread.sleep(millis);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//}
